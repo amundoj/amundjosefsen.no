cef93d3391382ba85344481700ac398bb9b37471
Amund Oliver Josefsen <amundojo@gmail.com>
2025-10-02 10:51:58 +0200

    Edit language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="no">
+    <html lang="eng">
       <body className={inter.className}>
         <NavBar />
         {children}
